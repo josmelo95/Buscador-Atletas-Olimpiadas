d655f0a631c7e645adb5885e8abe6b72a6468939
josmelo95 <168839415+josmelo95@users.noreply.github.com>
2024-09-06 11:52:31 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Legal",
+        descricao: "Rayssa Leal é uma skatista brasileira, conhecida como Fadinha, que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que marcou história nos Jogos Olímpicos. Com sua força, agilidade e determinação, ela conquistou o coração dos brasileiros e se tornou um dos maiores nomes da ginástica mundial.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+         tags: "judô judoka judo judoca"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é uma das maiores promessas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+         tags: "ginasta ginástica ouro"
+    }
+];
